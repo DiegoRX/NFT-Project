1ce6e3d3b44f763f9db9be22ad4fc011128ba1b8
DiegoRX <dio351a@gmail.com>
2023-03-22 15:23:38 -0500

    formated

--- components/molecules/Modal/Modal.tsx
+++ components/molecules/Modal/Modal.tsx
@@ -1,60 +1,55 @@
-import { Modal } from "antd";
-import React from "react";
-import { ModalProps } from "../modal.props";
-import { Modal as ModalUtil } from "../../../utils/model_utils";
+import { Modal } from 'antd';
+import React from 'react';
+import { ModalProps } from '../modal.props';
+import { Modal as ModalUtil } from '../../../utils/model_utils';
 
-export const ModalComponent = React.forwardRef(
-  (propsValues: ModalProps, ref) => {
-    const {
-      component: RenderInner,
-      props,
-      closable = true,
-      onClose = () => {},
-      closeModal = () => {},
-      isOpen,
-      width = '80vw',
-      height = '80vh',
-      title,
-      className = "modal-container",
-      modalFooter,
-      closeable = true,
-      closeIcon = false,
-      centered = true,
-    } = propsValues;
+export const ModalComponent = React.forwardRef((propsValues: ModalProps, ref) => {
+  const {
+    component: RenderInner,
+    props,
+    closable = true,
+    onClose = () => {},
+    closeModal = () => {},
+    isOpen,
+    width = '80vw',
+    height = '80vh',
+    title,
+    className = 'modal-container',
+    modalFooter,
+    closeable = true,
+    closeIcon = false,
+    centered = true,
+  } = propsValues;
 
-    const onModalClose = (isClose: any) => {
-      if (!closable) return;
-      if (isClose) {
-        closeModal();
-        onClose();
-      }
-      ModalUtil.close();
-    };
+  const onModalClose = (isClose: any) => {
+    if (!closable) return;
+    if (isClose) {
+      closeModal();
+      onClose();
+    }
+    ModalUtil.close();
+  };
 
-    return (
-      <Modal
-        open={isOpen}
-        title={title}
-        onCancel={onModalClose}
-        footer={modalFooter ? modalFooter : null}
-        width={width}
-        height={height}
-        className={className}
-        closeIcon={closeIcon}
-        closable={closeable}
-        centered={centered}
-        
+  return (
+    <Modal
+      open={isOpen}
+      title={title}
+      onCancel={onModalClose}
+      footer={modalFooter ? modalFooter : null}
+      width={width}
+      className={className}
+      closeIcon={closeIcon}
+      closable={closeable}
+      centered={centered}
+    >
+      <div
+        style={{
+          position: 'relative',
+          height: '100%',
+        }}
       >
-        <div
-       
-          style={{
-            position: "relative",
-            height:'100%'
-          }}
-        >
-          {RenderInner && <RenderInner inModal={true} {...props} />}
-        </div>
-      </Modal>
-    );
-  }
-);
+        {RenderInner && <RenderInner inModal={true} {...props} />}
+      </div>
+    </Modal>
+  );
+});
