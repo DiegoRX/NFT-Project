26e59ffcd01d9113a764bd1b80d5afea621670f9
DiegoRx <dio351a@gmail.com>
2023-04-13 15:13:05 -0500

    styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,13 +28,13 @@ const Home = () => {
 
 
   return (
-    <div className="h-screen">
+    <div className="h-auto">
       {/* Hero */}
       <section className="bg-mainDark">
         <div className="py-20 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
           <section className="banner-container">
             <div>
-              <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl dark:text-black">ROJO FOUNDER</h1>
+              <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl dark:text-black">ROJO FUNDADOR</h1>
               <p className="mb-8 text-lg font-normal text-justify text-gray-100 lg:text-xl ">
                 La primera colección nacional de NFTs estará basada en la tokenización de activos deportivos tangibles e intangibles, incluyendo bienes inmuebles y el metaverso. Estos activos se
                 mostrarán en los futuros estadios Arena Rojo que están planeados para ser construidos en varias ciudades alrededor del mundo.
@@ -46,7 +46,11 @@ const Home = () => {
               <button onClick={connectWallet} className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black  bg-white rounded-full  hover:bg-amber-600 ">
                 ¡COMPRAR AHORA!
               </button>
-   
+              <div className="p-4 flex justify-center">
+                <Link href="#">
+                  <Image width={50} height={50} src="https://sharktech-nft.vercel.app/MetaMask_Fox.svg.png" alt="" />
+                </Link>
+              </div>    
             </div>
             <div className="inline-flex justify-center items-center sm-none">
               <Image className='sm-none' width={300} height={300} src="https://maroon-imaginative-loon-41.mypinata.cloud/ipfs/QmcADNuny8YX71kx5bys7fFHVQSYKCiiGjSdt4gYRbBpvC/1.jpeg" alt="" />
@@ -67,14 +71,12 @@ const Home = () => {
             </section>
           {/* How Does It Work */}
           
-         
+          <div className="inline-flex justify-center items-center sm-none">
+              <Image className='sm-none' width={800} height={800} src="https://sharktech-nft.vercel.app/ESTADIO_AMERICA.jpeg" alt="" />
+            </div>
           <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
             <div className="club-cart__container">
-              <div className="flex justify-center">
-                <Link href="#">
-                  <Image width={50} height={50} src="https://sharktech-nft.vercel.app/MetaMask_Fox.svg.png" alt="" />
-                </Link>
-              </div>            
+                      
             </div>
           </div>
         </div>
--- pages/nft/[...nft].js
+++ pages/nft/[...nft].js
@@ -191,24 +191,24 @@ const NFT = () => {
         <section className="nft-container  h-1/3">
           <div className="flex flex-column justify-center item1 p-2 ">
             <h2 className="p-1 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">{NFT.name}</h2>
-            <div className="p-1 my-4   text-3x1 md:text-3xl lg:text-3xl   text-white  flex text-justify">{NFT.description}</div>
+            <div className="p-1 my-4   text-2xl md:text-2xl lg:text-2xl   text-white  flex text-justify">{NFT.description}</div>
 
-            <div className=" p-1  text-2x1 md:text-2xl lg:text-2xl text-white flex justify-between">
+            <div className=" pt-1  text-2xl md:text-2xl lg:text-2xl text-white flex justify-between">
               <span>Tonkens Minteados:</span>
             </div>
-            <div className=" p-1  text-2x1 md:text-3xl lg:text-3xl text-white flex justify-between">{minted}</div>
-            <div className="p-1  text-2x1 md:text-2xl lg:text-2xl text-white  flex justify-between">
+            <div className=" pb-1  text-2x1 md:text-2xl lg:text-2xl text-white flex justify-between">{minted}</div>
+            <div className="pt-1  text-2x1 md:text-2xl lg:text-2xl text-white  flex justify-between">
               <span>Precio en USD:</span>
             </div>
-            <div className="p-1  text-2x1 md:text-3xl lg:text-3xl text-white  flex text-right">${priceUSD}</div>
-            <div className="p-1  text-2x1 md:text-2xl lg:text-2xl text-white  flex text-right">
+            <div className="pb-1  text-2x1 md:text-2xl lg:text-2xl text-white  flex text-right">${priceUSD}</div>
+            <div className="pt-1  text-2x1 md:text-2xl lg:text-2xl text-white  flex text-right">
               <span>Precio en BNB:</span>
             </div>
-            <div className="p-1  text-2x1 md:text-3xl lg:text-3xl text-white  flex justify-between">{BNBPrice / (10 ** 18).toString()}</div>
-            <div className="p-1  text-2x1 md:text-2xl lg:text-2xl text-white  flex justify-between">
+            <div className="pb-1  text-2xl md:text-2xl lg:text-2xl text-white  flex justify-between">{BNBPrice / (10 ** 18).toString()}</div>
+            <div className="pt-1  text-2xl md:text-2xl lg:text-2xl text-white  flex justify-between">
               <span>{NFT.name} NFTs en tu wallet:</span>
             </div>
-            <div className="p-1  text-2x1 md:text-3xl lg:text-3xl text-white  flex justify-between">{accountBalance}</div>
+            <div className="pb-1  text-2x1 md:text-2xl lg:text-2xl text-white  flex justify-between">{accountBalance}</div>
             {/* <div className="p-2 text-white  flex justify-between">
             <span>Available</span>
             {NFT?.available.toString()}
@@ -223,7 +223,7 @@ const NFT = () => {
               <span>Dirección de contrato:</span>
             </div>
             <div className="  text-1x1 md:text-1xl lg:text-1xl text-white flex justify-between">{contractAddress}</div>
-            <Link href={`https://www.bscscan.com/address/${contractAddress}`} className="text-3x1 bg-mainDark text-rojo1 font-bold ">
+            <Link href={`https://www.bscscan.com/address/${contractAddress}`} className="text-2xl bg-mainDark text-rojo1 font-bold ">
               Ver en BNBSCAN
             </Link>
             <>
